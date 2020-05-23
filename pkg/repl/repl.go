5922a2f3896d2d67b18e83cb06f2b0fe7f61bf84
naoto0822 <n.h.in.m.h@gmail.com>
2020-05-24 08:44:58 +0900

    update repl

--- pkg/repl/repl.go
+++ pkg/repl/repl.go
@@ -6,11 +6,25 @@ import (
 	"io"
 
 	"github.com/naoto0822/monkey-interpreter/pkg/lexer"
-	"github.com/naoto0822/monkey-interpreter/pkg/token"
+	"github.com/naoto0822/monkey-interpreter/pkg/parser"
 )
 
 // PROMPT is >>
 const PROMPT = ">>"
+
+// MONKEY_FACE is so funny
+const MONKEY_FACE = `            __,__
+   .--.  .-"     "-.  .--.
+  / .. \/  .-. .-.  \/ .. \
+ | |  '|  /   Y   \  |'  | |
+ | \   \  \ 0 | 0 /  /   / |
+  \ '- ,\.-"""""""-./, -' /
+   ''-' /_   ^ ^   _\ '-''
+       |  \._   _./  |
+       \   \ '~' /   /
+        '._ '-=-' _.'
+           '-----'
+`
 
 // Start is starting repl
 func Start(in io.Reader, out io.Writer) {
@@ -26,9 +40,25 @@ func Start(in io.Reader, out io.Writer) {
 
 		line := scanner.Text()
 		l := lexer.New(line)
+		p := parser.New(l)
+		program := p.ParseProgram()
 
-		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-			fmt.Printf("%+v\n", tok)
+		if len(p.Errors()) > 0 {
+			printParseErrors(out, p.Errors())
+			continue
 		}
+
+		io.WriteString(out, program.String())
+		io.WriteString(out, "\n")
+	}
+}
+
+func printParseErrors(out io.Writer, errors []string) {
+	io.WriteString(out, MONKEY_FACE)
+	io.WriteString(out, "Woops! We ran into some monkey business here!\n")
+	io.WriteString(out, " parser errors:\n")
+
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
 	}
 }
