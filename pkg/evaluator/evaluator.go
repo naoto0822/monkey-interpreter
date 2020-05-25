19ec19b2bea368d4d356fd9fa9da4b56b942f498
naoto0822 <n.h.in.m.h@gmail.com>
2020-05-25 09:33:31 +0900

    eval bool / null

--- pkg/evaluator/evaluator.go
+++ pkg/evaluator/evaluator.go
@@ -5,6 +5,12 @@ import (
 	"github.com/naoto0822/monkey-interpreter/pkg/object"
 )
 
+var (
+	NULL  = &object.Null{}
+	TRUE  = &object.Boolean{Value: true}
+	FALSE = &object.Boolean{Value: false}
+)
+
 // Eval start parsing ast.Node
 func Eval(node ast.Node) object.Object {
 	switch node := node.(type) {
@@ -16,6 +22,8 @@ func Eval(node ast.Node) object.Object {
 	// expression
 	case *ast.IntegerLiteral:
 		return &object.Integer{Value: node.Value}
+	case *ast.Boolean:
+		return nativeBoolToBooleanObject(node.Value)
 	}
 
 	return nil
@@ -29,4 +37,12 @@ func evalStatements(stmts []ast.Statement) object.Object {
 	}
 
 	return result
+}
+
+func nativeBoolToBooleanObject(input bool) *object.Boolean {
+	if input {
+		return TRUE
+	}
+
+	return FALSE
 }
--- pkg/evaluator/evaluator_test.go
+++ pkg/evaluator/evaluator_test.go
@@ -23,6 +23,21 @@ func TestEvalIntegerExpression(t *testing.T) {
 	}
 }
 
+func TestEvalBooleanExpression(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected bool
+	}{
+		{"true", true},
+		{"false", false},
+	}
+
+	for _, tt := range tests {
+		obj := testEval(tt.input)
+		testBooleanObject(t, obj, tt.expected)
+	}
+}
+
 func testEval(input string) object.Object {
 	l := lexer.New(input)
 	p := parser.New(l)
@@ -44,4 +59,20 @@ func testIntegerObject(t *testing.T, obj object.Object, expected int64) bool {
 	}
 
 	return true
+}
+
+func testBooleanObject(t *testing.T, obj object.Object, expected bool) bool {
+	boolObj, ok := obj.(*object.Boolean)
+	if !ok {
+		t.Errorf("obj is not object.Boolean. got=%T", obj)
+		return false
+	}
+
+	if boolObj.Value != expected {
+		t.Errorf("boolObj.Value is not %t. got=%t", expected, boolObj.Value)
+		return false
+	}
+
+	return true
+
 }
